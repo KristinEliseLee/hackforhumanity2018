359accf0691ec7b5112e83a3b64313a2df20d938
Kristin Lee <serpent.kris@gmail.com>
2018-07-29 01:00:13 -0700

    Changed to dictionary for question functions, still need to implement last couple questions

--- server.py
+++ server.py
@@ -20,23 +20,89 @@ def start_quiz():
 @app.route("/setanswer")
 def set_answer():
     questnum = request.args.get("questnum")
-    if questnum == '0':
+
+    
+    def quest0():
         answer = request.args.get('status')
         session['status'] = answer
         if answer != 'no':
             return '1'
         return '2'
 
-    elif questnum =='1':
+    def quest1():
         answer = request.args.get('a-num')
         session['a-num'] = answer
         return '2'
 
-    elif questnum == '2':
+    def quest2():
         doc = request.args.getlist('doc')
         session['doc'] = doc
         return '3'
-    return 0
+
+    def quest3():
+        kids = request.args.get('kids')
+        session['kids'] = kids
+        if kids == 'yes':
+            return '4'
+        return '5'
+
+    def quest4():
+        kidscit = request.args.get('kidscit')
+        session['kidscit'] = kidscit
+        if kidscit == 'yes':
+            return '6'
+        return '5'
+
+    def quest5():
+        return '12'
+
+    quest_num_dict = {
+    '0': quest0,
+    '1': quest1,
+    '2': quest2,
+    '3': quest3,
+    '4': quest4,
+    '5': quest5,
+    }
+
+    return quest_num_dict[questnum]()
+
+    # if questnum == '0':
+    #     answer = request.args.get('status')
+    #     session['status'] = answer
+    #     if answer != 'no':
+    #         return '1'
+    #     return '2'
+
+    # elif questnum =='1':
+    #     answer = request.args.get('a-num')
+    #     session['a-num'] = answer
+    #     return '2'
+
+    # elif questnum == '2':
+    #     doc = request.args.getlist('doc')
+    #     session['doc'] = doc
+    #     return '3'
+
+    # elif questnum == "3":
+    #     kids = request.args.getlist('kids')
+    #     session['kids'] = kids
+    #     if kids == 'yes':
+    #         return '4'
+    #     return '5'
+
+    # elif questnum == "4":
+    #     kids_cit = request.args.getlist('kids-cit')
+    #     session['kids-cit'] = kids_cit
+    #     if kids_cit == 'yes':
+    #         return '6'
+    #     return '5'
+
+    # elif questnum == '5':
+    #     pass
+
+
+    # return 0
 
 
 @app.route('/results')
--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,53 +1,84 @@
 
 const questions = [
-{'html':`<h4>Which document do you currently have?</h4>
-<input type='radio' name='status' value='gc'>Green card
-<p id="gc.jpg" class="showpic">what does this look like?</p>
+`<h4>Which document do you currently have?</h4>
+<input type='radio' name='status' value='gc' required>Green card
+<p id="gc.jpg" class="showpic">What does this look like?</p>
 <input type='radio' name='status' value='v'>Visa
+<p id="visa.jpg" class="showpic">What does this look like?</p>
 <input type='radio' name='status' value='wp'>Work permit
+<p id="workpermit.jpg" class="showpic">What does this look like?</p>
 <input type='radio' name='status' value='no'>None of the above
 <input type='hidden' name='questnum' value='0'>
-<input type='submit'>`}
+<input type='submit'>`
 ,
-{'html':`<h4> Do you know your A-Number </h4>
-<input type='radio' name='a-num' value='yes'>yes
-<input type='radio' name='a-num' value='no'>no
+`<h4> Do you know your A-Number </h4>
+<p id="Anum.jpg" class="showpic">What does this look like?</p>
+<input type='radio' name='a-num' value='yes' required>yes
+<input type='radio' name='a-num' value='no' required>no
+<br>
 <input type='hidden' name='questnum' value='1'>
-<input type='submit'>`}
+<input type='submit'>`
 ,
-{'html':`<h4> Which documents do you currently have for yourself?</h4>
+`<h4> Which documents do you currently have for yourself?</h4>
 <input type='checkbox' name='doc' value='pass'>Passport of your home country
+<br>
 <input type='checkbox' name='doc' value='bc'>Birth Certificate
+<br>
 <input type='checkbox' name='doc' value='ml'>Marriage License
+<br>
 <input type='checkbox' name='doc' value='dl'>Driver's License or I.D. card
+<br>
 <input type='checkbox' name='doc' value='ssc'>Social Security Card or ITIN number
+<br>
 <input type='hidden' name='questnum' value='2'>
-<input type='submit'>`}
+<input type='submit'>`
+,
 
-]
+`<h4> Do you have any children under age 18?</h4>
+<input type='radio' name='kids' value='yes' required>yes
+<input type='radio' name='kids' value='no' required>no
+<input type='hidden' name='questnum' value='3'>
+<input type='submit'>`
+,
+
+`<h4> Is your child/children a U.S. citizen?</h4>
+<input type='radio' name='kidscit' value='yes' required>yes
+<input type='radio' name='kidscit' value='no' required>no
+<input type='hidden' name='questnum' value='4'>
+<input type='submit'>`
+
+,
+`<h4> Do you have these documents for your children?</h4>
+<p>U.S. Passport:</p>
+<input type='radio' name='kids-pass' value='yes' required>yes
+<input type='radio' name='kids-pass' value='no' required>no
+<br>
+<p>U.S. Birth Certificate</p>
+<input type='radio' name='kids-cert' value='yes' required>yes
+<input type='radio' name='kids-cert' value='no' required>no
+<input type='hidden' name='questnum' value='5'>
+<input type='submit'>`
+
+];
 
 function showPic(evt){
-    let target = evt.currentTarget
-    let targetID = target.id
-    $("#pic").html(`<img src='static/pictures/${targetID}'>`)
+    let targetID = evt.currentTarget.id;
+    $("#pic").html(`<img src='static/pictures/${targetID}'>`);
 }
 
 function loadQuestion(num){
     if (num >= questions.length){
-
-        window.location.replace("/results")
-        
+        window.location.replace("/results");
     }
-    $('#pic').html("")
-    $('#form').html(questions[num]['html'])
-    $(".showpic").on('click',showPic)
+    $('#pic').html("");
+    $('#form').html(questions[num]);
+    $(".showpic").on('click',showPic);
 }
 function getNextQuestion(evt){
     evt.preventDefault();
     let formValues = $('#form').serialize();
-    console.log(formValues)
-    $.get('/setanswer', formValues, loadQuestion)
-
+    console.log(formValues);
+    $.get('/setanswer', formValues, loadQuestion);
 }
 
 loadQuestion(0);
